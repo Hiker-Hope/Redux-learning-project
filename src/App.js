292888974d795c8302bf80c3384bea6623571761
Hope R <nadezhda.lkn@yahoo.com>
2018-11-19 19:04:12 +0300

    Fix mapDispatchToProps

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from 'react'
+import * as actions from './store/actions'
+import { connect } from 'react-redux'
+import './App.css'
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h3>The count is: {this.props.count}</h3>
+                <button onClick={this.props.addCount}>Add</button>
+                <button onClick={this.props.subtractCount}>Subtract</button>
+                <button onClick={this.props.resetCount}>Reset</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = state => {
+    return {
+        count: state.count
+    }
+}
+
+const mapDispatchToProps = {
+    addCount: actions.add,
+    subtractCount: actions.subtract,
+    resetCount: actions.reset
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App)
